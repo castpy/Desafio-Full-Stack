92e947b06ad29008d9af3f7aa85b37024d5ed7b8
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2023-02-25 06:16:19 -0300

    Ajustando erro de autenticação

--- dashboard/src/contexts/auth.js
+++ dashboard/src/contexts/auth.js
@@ -1,67 +1,69 @@
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from "react"
 
 export const AuthContext = createContext({})
 
 export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState()
+  const [user, setUser] = useState()
 
-    useEffect (() => {
-        const userToken = localStorage.getItem("user_token")
-        const usersStorage = localStorage.getItem("users_db")
+  useEffect (() => {
+    const userToken = localStorage.getItem("user_token")
+    const usersStorage = localStorage.getItem("users_bd")
 
-        if(userToken && usersStorage) {
-            const hasUser = JSON.parse(usersStorage)?.filter((user) => user.email === JSON.parse(userToken).email)
-
-            if (hasUser) setUser(hasUser[0]);
-        }
-    }, [])
-
-    const signin = (email, password) => {
-        const usersStorage = JSON.parse(localStorage.getItem("users_db"))
-        const hasUser = usersStorage?.filter((user) => user.email === email)
-
-        if(hasUser?.length){
-            if(hasUser[0].email === email && hasUser[0].password === password){
-                const token = Math.random().toString(36).substring(2)
-                localStorage.setItem("user_token", JSON.stringify({email, token}))
-                setUser({email, password})
-                return
-            } else {
-                return "E-mail ou senha incorretos"
-            }
-        }else {
-            return "Usuário não cadastrado"
-        }
+    if(userToken && usersStorage) {
+      const hasUser = JSON.parse(usersStorage)?.filter((user) => user.email === JSON.parse(userToken).email)
+      if (hasUser) setUser(hasUser[0]);
     }
+  }, [])
 
-    //Criando a função de inscrição
-    const signup = ({email, password}) => {
-        const usersStorage = JSON.parse(localStorage.getItem("users_db"))
-        const hasUser = usersStorage?.filter((user) => user.email === email)
+  const signin = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"))
 
-        if(hasUser?.length){
-            return "Já existe uma conta com este E-mail"
-        }
+    const hasUser = usersStorage?.filter((user) => user.email === email)
 
-        let newUser
-
-        if(usersStorage){
-            newUser = [...usersStorage, {email, password}]
-        } else {
-            newUser = [{email, password}]
-        }
-
-        localStorage.setItem("users_db", JSON.stringify(newUser))
-
+    if(hasUser?.length){
+      if(hasUser[0].email === email && hasUser[0].password === password){
+        const token = Math.random().toString(36).substring(2)
+        localStorage.setItem("user_token", JSON.stringify({email, token}))
+        setUser({email, password})
         return
+      } else {
+        return "E-mail ou senha incorretos"
+      }
+    }else {
+      return "Usuário não cadastrado"
+    }
+  }
+    //Criando a função de inscrição
+  const signup = (email, password) => {
+    const usersStorage = JSON.parse(localStorage.getItem("users_bd"))
+
+    const hasUser = usersStorage?.filter((user) => user.email === email)
+
+    if(hasUser?.length){
+      return "Já existe uma conta com este E-mail"
     }
 
+    let newUser
+
+    if(usersStorage){
+      newUser = [...usersStorage, {email, password}]
+    } else {
+      newUser = [{email, password}]
+    }
+
+    localStorage.setItem("users_bd", JSON.stringify(newUser))
+
+    return
+  }
     //Criando a função para deslogar o usuário
-    const signout = () => {
-        setUser(null)
-        localStorage.removeItem("user_token")
-    }
+  const signout = () => {
+    setUser(null)
+    localStorage.removeItem("user_token")
+  }
 
-    return <AuthContext.Provider value={{user, signed: !!user, signin, signup, signout}}>{children}</AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{user, signed: !!user, signin, signup, signout}}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
-
