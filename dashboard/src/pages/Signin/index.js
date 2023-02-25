5231cc72a61952fca7d9118310f0dd57a31d0386
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2023-02-25 05:40:49 -0300

    Página de login criada e estilizada

--- dashboard/src/pages/Signin/index.js
+++ dashboard/src/pages/Signin/index.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import * as C from './styles'
+import React, { useState } from 'react'
+import useAuth from '../../hook/userAuth'
+import Input from '../../Components/Input'
+import Button from '../../Components/Button'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signin = () => {
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [error, setError] = useState("")
+
+  const handleLogin = () => {
+    if (!email | !senha){
+      setError("Preencha todos os campos!")
+      return
+    }
+
+    const res = signin (email, senha)
+    if (res){
+      setError(res)
+      return
+    }
+
+    navigate("/home")
+  }
+
   return (
-    <div>Signin</div>
+    <C.Container>
+      <C.Label>Login</C.Label>
+      <C.Content>
+        <Input 
+          type="email"
+          placeholder="Digite seu E-mail"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+
+        <Input 
+          type="password"
+          placeholder="Digite sua senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+        <C.LabelError>{error}</C.LabelError>
+        <Button Text="Entrar" onClick={handleLogin} />
+        <C.LabelSignup>
+          Não tem conta?
+          <C.Strong>
+            <Link to="/singup">&nbsp;Registre-se</Link>
+          </C.Strong>
+        </C.LabelSignup>
+      </C.Content>
+    </C.Container>
   )
 }
 
