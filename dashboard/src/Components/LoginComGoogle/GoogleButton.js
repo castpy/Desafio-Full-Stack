c5a7c1ca674099ec804452a17ee1414aa493f8ef
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2023-02-26 13:14:56 -0300

    Google button finalizado

--- dashboard/src/Components/LoginComGoogle/GoogleButton.js
+++ dashboard/src/Components/LoginComGoogle/GoogleButton.js
@@ -2,20 +2,22 @@ import React, { useEffect } from 'react';
 import { GoogleLogin } from 'react-google-login';
 import { gapi } from 'gapi-script'
 
+//Id de acesso a API do google login
 const clientId ='928406916720-tqvljcjn4q1kkbae34gldo93ktdpo0kp.apps.googleusercontent.com'
 
 //ajustando bug de popup fechado - gapi
-
 const GoogleButton = () => {
-    useEffect(() => {
-        gapi.load("client:auth2", () => {
-          gapi.auth2.init({clientId:clientId})
-        })
-      }, [])
+  useEffect(() => {
+      gapi.load("client:auth2", () => {
+        gapi.auth2.init({clientId:clientId})
+      })
+    }, [])
+  //Resposta da API - Dados do usuário em response
   const responseGoogle = (response) => {
     console.log(response);
-        //
-        window.location.href = "/home";
+
+    //redirecionando o usuário
+    window.location.href = "/home";
   };
 
   return (
