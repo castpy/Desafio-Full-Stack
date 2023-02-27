27661a9dadb22b7644d38401b196ab0e95896657
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2023-02-27 13:15:29 -0300

    Update style.js

--- dashboard/src/Components/Grid/style.js
+++ dashboard/src/Components/Grid/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Table = styled.table`
-    width: 90%;
+    width: 100%;
     background-color: #fff;
     padding: 20px;
     box-shadow: 0px 0px 5px #ccc;
