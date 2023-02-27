62c2517ef95e5e8ff3ecf3608ca3711a9cbc7029
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2023-02-27 13:20:33 -0300

    Update style.js

--- dashboard/src/Components/GridItem/style.js
+++ dashboard/src/Components/GridItem/style.js
@@ -4,7 +4,7 @@ export const Tr = styled.tr``
 
 export const Td = styled.td`
     padding-top: 15px;
-    text-align: center;
+    text-align: ${(props) => (props.alignCenter ? "center" : "start")};
     word-break: break-all;
 
     svg {
